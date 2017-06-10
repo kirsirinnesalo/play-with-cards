435182e1871769680db3e1dc3dc14a3ed816de9b
Kirsi Rinnesalo <kirsi.rinnesalo@gmail.com>
2017-06-10 23:07:08 +0300

    A little demo of draggable card

---
+++ src/main/java/com/github/kirsirinnesalo/cards/control/CardView.java
@@ -0,0 +1,68 @@
+package com.github.kirsirinnesalo.cards.control;
+
+import com.github.kirsirinnesalo.cards.Card;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class CardView extends ImageView {
+
+    private final Card card;
+    private final Image face;
+    private final Image back;
+    private boolean faceUp = false;
+
+    public CardView(Card card, int cardWidth, int cardHeight) {
+        this.card = card;
+        face = createFaceImage();
+        back = createBackImage();
+        setImage(back);
+        setFitWidth(cardWidth);
+        setFitHeight(cardHeight);
+    }
+
+    public boolean isFaceUp() {
+        return faceUp;
+    }
+
+    public void turnFaceDown() {
+        faceUp = false;
+        setImage(back);
+    }
+
+    public void turnFaceUp() {
+        faceUp = true;
+        setImage(face);
+    }
+
+    public void turnFace() {
+        if (isFaceUp()) {
+            turnFaceDown();
+        } else {
+            turnFaceUp();
+        }
+    }
+
+    private Image createFaceImage() {
+        return loadCardImage(card.rank.asText() + "_of_" + card.suit.asText());
+    }
+
+    private Image createBackImage() {
+        return loadCardImage("back");
+    }
+
+    private Image loadCardImage(String cardName) {
+        String imagePath = "cards/" + cardName + ".png";
+        return getImage(imagePath);
+    }
+
+    private Image getImage(String imagePath) {
+        return new Image(getResource(imagePath));
+    }
+
+    private String getResource(String imagePathName) {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        return classLoader.getResource(imagePathName).toExternalForm();
+    }
+
+}
---
+++ src/main/java/com/github/kirsirinnesalo/cards/demo/CardDemo.java
@@ -0,0 +1,34 @@
+package com.github.kirsirinnesalo.cards.demo;
+
+import com.github.kirsirinnesalo.cards.Card;
+import com.github.kirsirinnesalo.cards.control.CardView;
+import com.github.kirsirinnesalo.cards.control.DraggableCardView;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class CardDemo extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Card Demo");
+        stage.setScene(createScene());
+        stage.show();
+    }
+
+    private Scene createScene() {
+        StackPane table = new StackPane();
+        table.getChildren().add(createCard());
+        return new Scene(table, 600, 600);
+    }
+
+    private CardView createCard() {
+        Card queenOfHearts = new Card(Card.Rank.QUEEN, Card.Suit.HEARTS);
+        CardView cardView = new DraggableCardView(queenOfHearts, 300, 400);
+        cardView.setOnMouseClicked(event -> cardView.turnFace());
+        return cardView;
+    }
+
+}
